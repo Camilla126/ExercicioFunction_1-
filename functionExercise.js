05bc2e74e371f1a396cc7ac6dcda28b6521ffc28
CamillaCarvalho <Camillacarvalho037@gmail.com>
2024-04-18 12:17:56 -0300

    Subindo projeto

---
+++ functionExercise.js
@@ -0,0 +1,19 @@
+const balance = 100;
+
+function sumitem(value) {
+    return balance + value
+}
+
+function subitem(value) {
+    return balance - value
+}
+
+function multitem(value) {
+    return balance * value
+}
+
+function divitem(value) {
+    return balance / value
+}
+
+console.log(sumitem(7))
